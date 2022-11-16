d65331f325fe3fbf7b24e9c3bffe32709a155772
Hanh Dao <4lm0nd.dh@gmail.com>
2022-11-16 15:10:56 -0500

    ignore - test push

--- TestCases/Railway/Test_draft.java
+++ TestCases/Railway/Test_draft.java
@@ -14,16 +14,6 @@ public class Test_draft {
 
     public static void main(String[] args) {
         logger.info("TC15_Verify_User can open Book ticket page by clicking on Book ticket link in Train timetable page");
-        logger.info("Step 1: Login");
-        generalPage.openSite(Constant.RAILWAY_SITE);
-        loginPage.login(Constant.USER_NAME, Constant.PASSWORD);
-        logger.info("Step 2: Go to Timetable page");
-        generalPage.goToTab("Timetable");
-        generalPage.scrollDown();
-        logger.info("Step 3: Click on book ticket button routed from Phan Thiet to Sai Gon");
-        timetablePage.selectTrainFromTrainTimeTable("Huế", "Nha Trang");
-        logger.info("Check book ticket page is loaded with correct Depart From and Arrive At");
-        utilities.checkSelectedItemList(bookTicketPage.getListDepartFrom(), "Huế");
-        utilities.checkSelectedItemList(bookTicketPage.getListArriveAt(), "Nha Trang");
+
     }
 }
